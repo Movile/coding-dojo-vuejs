9fbcb139c9c153e5c5868f44fbab8c325eb97b9c
santiagogustavo <santiago.gustavo.mail@gmail.com>
2020-03-07 16:20:09 -0300

    feature(cookie) Add initial components and main logics

---
+++ src/components/Cookie.vue
@@ -0,0 +1,32 @@
+<template>
+  <img class="cookie" src="../assets/cookie.png" @click="handleClickCookie" />
+</template>
+
+<script>
+export default {
+  name: "Cookie",
+  methods: {
+    handleClickCookie() {
+      this.$emit("clicked");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.cookie {
+  display: block;
+  height: 200px;
+  width: 200px;
+  transition: transform 200ms ease;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+
+  &:active {
+    transform: scale(0.95);
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <section class="home__content">
+      <h1>COOKIE CLICKER MUITO LOCO</h1>
+      <Cookie class="home__cookie" @clicked="onClickCookie" />
+      <section class="home__score"><b>CLICKS:</b> {{ clicks }}</section>
+    </section>
+    <button @click="onClickReset">
+      RESET
+    </button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Cookie from "@/components/Cookie.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Cookie
+  },
+  data() {
+    return {
+      clicks: 0
+    };
+  },
+  methods: {
+    onClickCookie() {
+      this.clicks += 1;
+      this.$cookies.set("clicks", this.clicks);
+    },
+    onClickReset() {
+      this.clicks = 0;
+      this.$cookies.remove("clicks");
+    }
+  },
+  created() {
+    this.clicks = Number(this.$cookies.get("clicks"));
   }
 };
 </script>
+
+<style lang="scss">
+.home {
+  &__content {
+    margin: auto;
+  }
+  margin: auto;
+  &__cookie {
+    margin: 32px auto;
+  }
+}
+</style>
